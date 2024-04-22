947ed0c7e2e34acae259158e04a21113fd5c3361
MohamedHabibSoula <habib.soula@value.com.tn>
2024-04-22 14:03:53 +0200

    Create a findByMoile abstract Class

--- src/main/java/com/eazybytes/accounts/repository/CustomerRepository.java
+++ src/main/java/com/eazybytes/accounts/repository/CustomerRepository.java
@@ -4,7 +4,20 @@ import com.eazybytes.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    // We use optional because the phone number might or might not have a mobile number
+    // this is an *abstract* class
+    // SpringJPA will understand that we are trying to execute a select query by the keyword "findBy",
+    // the column name must match the calling of the class, not CASE SENSITIVE
+    // you can use and if you want to have multiple columns (findByMobileNumberAnd)
 
+    /**
+     *
+     * Derived Name method
+     *
+     */
+    Optional<Customer> findByMobileNumber(String mobileNumber);
 }
